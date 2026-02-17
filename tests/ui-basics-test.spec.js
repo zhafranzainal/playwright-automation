b02b4b9c3dddff3538d44fb19e22c0097be085e1
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2026-02-17 21:26:00 +0800

    Extract text content

--- tests/ui-basics-test.spec.js
+++ tests/ui-basics-test.spec.js
@@ -1,18 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('Browser Context Playwright test', async ({ browser }) => {
+test.only('Browser Context Playwright test', async ({ browser }) => {
 
     // chrome - plugins / cookies
     const context = await browser.newContext();
     const page = await context.newPage();
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    await page.locator('#username').fill('rahulshetty');
+    await page.locator("[type='password']").fill('learning');
+    await page.locator('#signInBtn').click();
+    console.log(await page.locator("[style*='block']").textContent());
 
 });
 
 
-test.only('Page Playwright test', async ({ page }) => {
+test('Page Playwright test', async ({ page }) => {
 
-    await page.goto("https://google.com")
-    await expect(page).toHaveTitle('Google')
+    await page.goto("https://google.com");
+    await expect(page).toHaveTitle('Google');
 
 });
